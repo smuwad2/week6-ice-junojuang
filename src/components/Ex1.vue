e6aec50e1d480e5f645d2cc108fa4345c1142066
Justin Poh <justinpohtm@gmail.com>
2025-10-24 18:48:15 +0800

    Refactor Ex1.vue to use an array for fruits

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,24 +1,22 @@
 <script>
     export default { 
         // Add Code Here
-        // challenge: use array  
-        data() {
-            return {
-                message: "Choose your favorite fruit: ", 
-                fruit1: "apple",
-                fruit2: "orange",
-                fruit3: "peach",
+        // challenge: use array
+        data(){
+            return{
+        fruits: ["apple", "orange", "banana"]
             }
-        }
+        }  
     }
 </script>
 
 <template>
     <div id="app">
-        {{ message }} </br>
-        {{ fruit1 }} <input type="radio" name="fruit">
-        {{ fruit2 }} <input type="radio" name="fruit">
-        {{ fruit3 }} <input type="radio" name="fruit">
+        <h3>Using Array</h3>
+        <span v-for="fruit in fruits" :key="fruit">
+            {{ fruit }}<input type="radio" name="fruit">
+        </span>
+    
     </div>
 </template>
 
